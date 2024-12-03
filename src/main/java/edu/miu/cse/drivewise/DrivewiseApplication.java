0151ad70d498ddcb5c01fb1dfdab8f2dd85b1676
ThiriMon1 <108010913+ThiriMon1@users.noreply.github.com>
2024-12-03 16:00:51 -0600

    clean DWapplication.java

--- src/main/java/edu/miu/cse/drivewise/DrivewiseApplication.java
+++ src/main/java/edu/miu/cse/drivewise/DrivewiseApplication.java
@@ -9,42 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class DrivewiseApplication {
-    private final MakeService makeService;
-    private final CarModelService modelService;
 
     public static void main(String[] args) {
         SpringApplication.run(DrivewiseApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner commandLineRunner() {
-//        return args -> {
-//
-////            List<String> modelNames = Arrays.asList("Camry","Corolla","RAV4", "RAV4 Hybrid");
-//            MakeRequestDto makeRequestDto = new MakeRequestDto("Toyota");
-//            Optional<MakeResponseDto> makeResponseDtoOptional = makeService.saveMake(makeRequestDto);
-//
-//
-////            List<CarModelRequestDto> modelRequestDtos = modelNames.stream()
-////                    .map(modelName->new CarModelRequestDto(modelName, makeRequestDto))
-////                    .collect(Collectors.toList());
-//            CarModelRequestDto modelRequestDto1 = new CarModelRequestDto("Camry", makeResponseDtoOptional);
-//            modelService.saveModel(modelRequestDto1);
-//            CarModelRequestDto modelRequestDto2 = new CarModelRequestDto("Corolla", makeRequestDto);
-//            modelService.saveModel(modelRequestDto2);
-//            CarModelRequestDto modelRequestDto3 = new CarModelRequestDto("RAV4", makeRequestDto);
-//            modelService.saveModel(modelRequestDto2);
-//
-//
-////            List<String> bmwmodelNames = Arrays.asList("1 Series","2 Series","3 Series", "3 Series Gran Turismo");
-////            List<CarModelRequestDto> bmwmodelRequestDtos = bmwmodelNames.stream()
-////                    .map(CarModelRequestDto::new)
-////                    .collect(Collectors.toList());
-////            MakeRequestDto bmwmakeRequestDto = new MakeRequestDto("BMW",bmwmodelRequestDtos);
-////            System.out.println("Make request: " + bmwmakeRequestDto);
-////            makeService.saveMake(bmwmakeRequestDto);
-//
-//        };
-  //  }
 
 }
