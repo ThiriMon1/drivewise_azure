0bf50b803ae03807a1dbc17b4071ea4eeee32429
ThiriMon1 <108010913+ThiriMon1@users.noreply.github.com>
2024-12-03 19:49:16 -0600

    remove context in test file

--- src/test/java/edu/miu/cse/drivewise/controller/CarReviewControllerTest.java
+++ src/test/java/edu/miu/cse/drivewise/controller/CarReviewControllerTest.java
@@ -25,78 +25,78 @@ import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class CarReviewControllerTest {
-//    @Mock
-//    private CarReviewService carReviewService;
-//    @InjectMocks
-//    private CarReviewController carReviewController;
-//
-//    @Test
-//    void createReview_validInput_returnCreatedReview() {
-//        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
-//        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
-//        ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
-//
-//        Mockito.when(carReviewService.createCarReview(Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
-//        ResponseEntity<CarReviewResponseDto> response=carReviewController.createReview(carReviewRequestDto);
-//
-//        assert response.getStatusCode()== HttpStatus.CREATED;
-//
-//
-//    }
-//
-//    @Test
-//    void getAllReviews_returnAllReviews() {
-//        List<CarReviewResponseDto> carReviewResponseDtoList=new ArrayList<>();
-//        carReviewResponseDtoList.add(new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
-//                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now()));
-//
-//        Mockito.when(carReviewService.getAllCarReview()).thenReturn( carReviewResponseDtoList);
-//
-//        ResponseEntity<List<CarReviewResponseDto>> response=carReviewController.getAllReviews();
-//        assert response.getStatusCode()== HttpStatus.OK;
-//    }
-//
-//    @Test
-//    void getFilteredReviews_returnFilteredReviews() {
-//        List<CarReviewResponseDto> carReviewResponseDtoList=new ArrayList<>();
-//        carReviewResponseDtoList.add(new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
-//                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now()));
-//
-//        Mockito.when(carReviewService.getCarReviewByMakeModelYear("toyota","corolla",2019)).thenReturn( carReviewResponseDtoList);
-//
-//        ResponseEntity<List<CarReviewResponseDto>> response=carReviewController.getFilteredReviews("toyota","corolla",2019);
-//        assert response.getStatusCode()== HttpStatus.OK;
-//    }
-//
-//    @Test
-//    void updateReviewPartially_returnUpdatedReview() {
-//        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
-//        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
-//                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
-//
-//        Mockito.when(carReviewService.updateCarReviewPartially(Mockito.eq(11L),Mockito.eq(11L),Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
-//        ResponseEntity<CarReviewResponseDto> response=carReviewController.updateReviewPartially(11L,11L,carReviewRequestDto);
-//
-//        assert response.getStatusCode()== HttpStatus.OK;
-//    }
-//
-//    @Test
-//    void updateReview_returnUpdatedReview() {
-//        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
-//        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
-//                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
-//
-//        Mockito.when(carReviewService.updateCarReview(Mockito.eq(11L),Mockito.eq(10L),Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
-//        ResponseEntity<CarReviewResponseDto> response=carReviewController.updateReview(11L,10L,carReviewRequestDto);
-//
-//        assert response.getStatusCode()== HttpStatus.OK;
-//    }
+    @Mock
+    private CarReviewService carReviewService;
+    @InjectMocks
+    private CarReviewController carReviewController;
 
-//    @Test
-//    void deleteReview_returnNoContent() {
-//        Mockito.doNothing().when(carReviewService).deleteCarReview(1L,Mockito.anyLong());
-//
-//        ResponseEntity<Void> response=carReviewController.deleteReview(1L,11L);
-//        assert response.getStatusCode()== HttpStatus.NO_CONTENT;
-//    }
+    @Test
+    void createReview_validInput_returnCreatedReview() {
+        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
+        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
+                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
+
+        Mockito.when(carReviewService.createCarReview(Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
+        ResponseEntity<CarReviewResponseDto> response=carReviewController.createReview(carReviewRequestDto);
+
+        assert response.getStatusCode()== HttpStatus.CREATED;
+
+
+    }
+
+    @Test
+    void getAllReviews_returnAllReviews() {
+        List<CarReviewResponseDto> carReviewResponseDtoList=new ArrayList<>();
+        carReviewResponseDtoList.add(new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
+                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now()));
+
+        Mockito.when(carReviewService.getAllCarReview()).thenReturn( carReviewResponseDtoList);
+
+        ResponseEntity<List<CarReviewResponseDto>> response=carReviewController.getAllReviews();
+        assert response.getStatusCode()== HttpStatus.OK;
+    }
+
+    @Test
+    void getFilteredReviews_returnFilteredReviews() {
+        List<CarReviewResponseDto> carReviewResponseDtoList=new ArrayList<>();
+        carReviewResponseDtoList.add(new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
+                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now()));
+
+        Mockito.when(carReviewService.getCarReviewByMakeModelYear("toyota","corolla",2019)).thenReturn( carReviewResponseDtoList);
+
+        ResponseEntity<List<CarReviewResponseDto>> response=carReviewController.getFilteredReviews("toyota","corolla",2019);
+        assert response.getStatusCode()== HttpStatus.OK;
+    }
+
+    @Test
+    void updateReviewPartially_returnUpdatedReview() {
+        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
+        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
+                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
+
+        Mockito.when(carReviewService.updateCarReviewPartially(Mockito.eq(11L),Mockito.eq(11L),Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
+        ResponseEntity<CarReviewResponseDto> response=carReviewController.updateReviewPartially(11L,11L,carReviewRequestDto);
+
+        assert response.getStatusCode()== HttpStatus.OK;
+    }
+
+    @Test
+    void updateReview_returnUpdatedReview() {
+        CarReviewRequestDto carReviewRequestDto=new CarReviewRequestDto("sdfdf",4,new MakeRequestDto("toyota"),new CarModelRequestDto("corolla"),2019,new CustomerRequestDto("123@gmail.com"));
+        CarReviewResponseDto carReviewResponseDto=new CarReviewResponseDto(11L,"dfdf great", 4, new CustomerResponseDto(1L,"u1","smith","u1@gmail.com"),new MakeResponseDto("toyota")
+                ,new CarModelResponseDto("corolla"), 2018, LocalDateTime.now());
+
+        Mockito.when(carReviewService.updateCarReview(Mockito.eq(11L),Mockito.eq(10L),Mockito.any(CarReviewRequestDto.class))).thenReturn(Optional.of(carReviewResponseDto));
+        ResponseEntity<CarReviewResponseDto> response=carReviewController.updateReview(11L,10L,carReviewRequestDto);
+
+        assert response.getStatusCode()== HttpStatus.OK;
+    }
+
+    @Test
+    void deleteReview_returnNoContent() {
+        Mockito.doNothing().when(carReviewService).deleteCarReview(Mockito.anyLong(),Mockito.anyLong());
+
+        ResponseEntity<Void> response=carReviewController.deleteReview(1L,11L);
+        assert response.getStatusCode()== HttpStatus.NO_CONTENT;
+    }
 }
